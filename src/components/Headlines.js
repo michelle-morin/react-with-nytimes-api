ba7abe37b150bdf5058d701636789ecb15606dd1
Michelle Morin <morin2m@gmail.com>
2020-05-09 17:43:09 -0700

    update headlines component to use redux instead of local state

--- src/components/Headlines.js
+++ src/components/Headlines.js
@@ -1,24 +1,22 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { makeApiCall } from './actions';
 
 class Headlines extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      headlines: []
-    };
   }
 
   componentDidMount() {
-    this.makeApiCall()
+    const { dispatch } = this.props;
+    dispatch(makeApiCall());
   }
 
   render() {
-    const { error, isLoaded, headlines } = this.state;
+    const { error, isLoading, headlines } = this.props;
     if (error) {
       return <React.Fragment>Error: {error.message}</React.Fragment>;
-    } else if (!isLoaded) {
+    } else if (!isLoading) {
       return <React.Fragment>Loading...</React.Fragment>;
     } else {
       return (
@@ -38,4 +36,12 @@ class Headlines extends React.Component {
   }
 }
 
-export default Headlines;
+const mapStateToProps = state => {
+  return {
+    headlines: state.headlines,
+    isLoading: state.isLoading,
+    error: state.error
+  }
+}
+
+export default connect(mapStateToProps)(Headlines);
